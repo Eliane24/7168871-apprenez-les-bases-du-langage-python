54fd485f90b4cebbad8eec8f0a5ce2c1179a14fa
Eliane24 <74274423+Eliane24@users.noreply.github.com>
2025-09-09 12:39:31 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,8 @@
-# Écrivez votre code ici !
+fruits= ["pomme", "banane","orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1]= "ananas"
+N=len(fruits)
+print(N)
+fruits.sort() 
+print(fruits)
